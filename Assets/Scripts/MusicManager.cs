eff2c6f910b05f7290602ba8a040250c8a13feae
Debayan Basu <debayanbasu11@gmail.com>
2020-10-23 20:36:10 +0530

    committing initial project files

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnLevelWasLoaded(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            AudioManager.instance.PlayMusic(mainTheme, 3);
+        }
+    }
+
+    void OnLevelWasLoaded(int sceneIndex){
+        string newSceneName = SceneManager.GetActiveScene().name;
+        if(newSceneName != sceneName){
+            sceneName = newSceneName;
+            Invoke("PlayMusic", .2f);
+        }
+    }
+
+    void PlayMusic(){
+        AudioClip clipToPlay = null;
+
+        if(sceneName == "Menu"){
+            clipToPlay = menuTheme;
+        }else if(sceneName == "Game"){
+            clipToPlay = mainTheme;
+        }
+
+        if(clipToPlay != null){
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
+        }
+    }
+}
